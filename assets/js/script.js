f07812258acf2b2ea1e8aa77a29123ac0101ebb3
T <endlesstruffle80@gmail.com>
2022-09-24 04:25:21 -0700

    adds ability to display time blocks with time, text area, and button. Also text area background color changes with time of day. Day is displayed at the top of the page.

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+const $dateDisplay = document.querySelector("#currentDay");
+const $schedule = document.querySelector(".container");
+const date = moment();
+let hour = 7;
+
+$dateDisplay.textContent = date.format("dddd, MMMM Do");
+
+for(let x = 0; x < 14; x++) {
+    const $timeDiv = document.createElement("div");
+    const $scheduleEvent = document.createElement("form");
+    const $timeDisplay = document.createElement("p");
+    const $timeTxtArea = document.createElement("textarea");
+    const $timeBtn = document.createElement("button")
+
+    $scheduleEvent.setAttribute("class", "row");
+    $timeDisplay.setAttribute("class", "time-block hour col-1");
+    $timeDisplay.setAttribute("id", `${hour}`);
+    $timeTxtArea.setAttribute("class", "description col-10");
+    $timeBtn.setAttribute("type", "submit");
+    $timeBtn.setAttribute("value", "Submit");
+    $timeBtn.setAttribute("class", "saveBtn col-1");
+
+    if(hour < 12) {
+        $timeDisplay.textContent = `${hour} am`;
+    } else if(hour >= 12) {
+        if(hour === 12){
+            $timeDisplay.textContent = `${hour} pm`;
+        } else {
+            $timeDisplay.textContent = `${hour-12} pm`;
+        }
+    }
+
+    hour++;
+
+    $scheduleEvent.appendChild($timeDisplay);
+    $scheduleEvent.appendChild($timeTxtArea)
+    $scheduleEvent.appendChild($timeBtn);
+    $timeDiv.appendChild($scheduleEvent);
+    $schedule.appendChild($timeDiv);
+}
+
+function txtAreaColor() {
+    const timeArray = document.querySelectorAll(".time-block");
+    const txtArray = document.querySelectorAll(".description");
+
+    for(let i = 0; i < timeArray.length; i++) {
+        let currentHour = parseInt(date.format("H"));
+        let listedHour = parseInt(timeArray[i].getAttribute('id'));
+        
+        if(listedHour > currentHour) {
+            txtArray[i].setAttribute("class", "description col-10 future");
+        } else if (listedHour == currentHour) {
+            txtArray[i].setAttribute("class", "description col-10 present");
+        } else if (listedHour < currentHour) {
+            txtArray[i].setAttribute("class", "description col-10 past");
+        }
+    }
+
+}
+
+txtAreaColor();
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    
+}
